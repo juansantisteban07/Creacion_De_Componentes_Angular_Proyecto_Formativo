1389214bebf42d51d42f72ffbdc3c1fdc866a544
Juan Sebastian <jdiazsantisteban77@gmail.com>
2026-09-26 00:55:17 +0000

    prototipo al 90%

--- miradent-frontend/src/app/components/citas/agendar-cita/agendar-cita.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-agendar-cita',
-  imports: [],
-  templateUrl: './agendar-cita.html',
-  styleUrl: './agendar-cita.css',
-})
-export class AgendarCita {}
